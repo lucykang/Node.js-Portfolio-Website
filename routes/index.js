3677654e9d41c1c116b99de89b7196f1c5dda239
Lucy <HaeYeon.Kang@MyGeorgian.ca>
2016-02-03 22:06:52 -0500

    modified custom css file. added temporary logo. modified pages contents.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,26 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('pages/index', { title: 'HAE YEON KANG' });
-  //res.render('pages/index');
+});
+
+/* about me */
+router.get('/aboutme', function(req, res, next){
+    res.render('pages/aboutme', { title: 'ABOUT ME'});
+});
+
+/* portfolio */
+router.get('/portfolio', function(req, res, next){
+   res.render('pages/portfolio', { title: 'PORTFOLIO'}); 
+});
+
+/* services */
+router.get('/services', function(req, res, next){
+   res.render('pages/services', { title: 'SERVICES'}); 
+});
+
+/* contact me */
+router.get('/contactme', function(req, res, next){
+    res.render('pages/contactme', { title: 'CONTACT ME'});
 });
 
 module.exports = router;
