ace257ab348f707def4681ee9c52945b0e9f36ca
Lucy <HaeYeon.Kang@MyGeorgian.ca>
2016-02-06 15:51:53 -0500

    changed site structure

--- app.js
+++ app.js
@@ -26,9 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.listen(3000);
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
